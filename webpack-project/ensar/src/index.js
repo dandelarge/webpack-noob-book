1920e97a9cd8360a3b55cca46af5a3868961423e
ENSAR DOGAN <doganermansur@icloud.com>
2023-12-14 15:18:18 +0100

    add all configurations

--- webpack-project/ensar/src/index.js
+++ webpack-project/ensar/src/index.js
@@ -1,1 +1,9 @@
-console.log("ensar");
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app"));
+root.render(<h1>Hello, world</h1>);
--- webpack-project/ensar/webpack.config.js
+++ webpack-project/ensar/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  entry: "./dist/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
   },
 };
